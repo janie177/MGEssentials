5cd3669f1f192235db123b80db6e677583124050
Jan <janie177@hotmail.com>
2015-01-02 01:56:46 +0100

    Removed old muted map.

--- src/main/java/com/minegusta/mgessentials/listener/ChatListener.java
+++ src/main/java/com/minegusta/mgessentials/listener/ChatListener.java
@@ -1,7 +1,6 @@
 package com.minegusta.mgessentials.listener;
 
 
-import com.minegusta.mgessentials.command.MuteCommand;
 import com.minegusta.mgessentials.data.TempData;
 import com.minegusta.mgessentials.util.RainBowStringMaker;
 import org.bukkit.ChatColor;
@@ -16,12 +15,6 @@ public class ChatListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onChat(AsyncPlayerChatEvent e) {
         String p = e.getPlayer().getName();
-        String m = e.getMessage();
-        if (MuteCommand.mutedMap.containsKey(p) && MuteCommand.mutedMap.get(p)) {
-            e.setCancelled(true);
-            e.getPlayer().sendMessage(ChatColor.RED + "You are muted. This will be undone when a moderator undoes it or at a server reboot. (9AM GMT).");
-            e.getPlayer().sendMessage(ChatColor.GREEN + "Muted for a false reason? Visit: " + ChatColor.AQUA + "http://forum.minegusta.com/");
-        }
         if (TempData.massMute && !e.getPlayer().hasPermission("minegusta.massmute.exempt")) {
             e.getPlayer().sendMessage(ChatColor.RED + "The server is in muted mode! Only staff can talk.");
             e.setCancelled(true);
@@ -41,17 +34,6 @@ public class ChatListener implements Listener {
 
     @EventHandler
     public void onSpamChat(PlayerCommandPreprocessEvent e) {
-        String p = e.getPlayer().getName();
-        if (MuteCommand.mutedMap.containsKey(p) && MuteCommand.mutedMap.get(p)) {
-            String message = e.getMessage();
-            message = message.substring(1);
-            String[] args = message.split("\\s+");
-            if (args[0].equalsIgnoreCase("me") || args[0].equalsIgnoreCase("tell") || args[0].equalsIgnoreCase("r") || args[0].equalsIgnoreCase("msg") || args[0].equalsIgnoreCase("whisper") || args[0].toLowerCase().contains("bukkit") || args[0].equalsIgnoreCase("poke") || args[1].equalsIgnoreCase("name") || args[1].equalsIgnoreCase("create")) {
-                e.setCancelled(true);
-                e.getPlayer().sendMessage(ChatColor.RED + "You are muted. This will be undone when a moderator undoes it or at a server reboot. (9AM GMT).");
-                e.getPlayer().sendMessage(ChatColor.GREEN + "Muted for a false reason? Visit: " + ChatColor.AQUA + "http://forum.minegusta.com/");
-            }
-        }
         if (TempData.massMute && !e.getPlayer().hasPermission("minegusta.massmute.exempt")) {
             String message = e.getMessage();
             message = message.substring(1);
