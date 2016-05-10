619a4c884b7b88199b14cf014360ce4537358ba2
Jan <janie177@hotmail.com>
2016-05-10 19:41:59 +0200

    Fix hat command.

--- src/main/java/com/minegusta/mgessentials/command/HatCommand.java
+++ src/main/java/com/minegusta/mgessentials/command/HatCommand.java
@@ -1,6 +1,7 @@
 package com.minegusta.mgessentials.command;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,8 +14,8 @@ public class HatCommand implements CommandExecutor {
 		if (sender instanceof Player) {
 			Player p = (Player) sender;
 			if (sender.hasPermission("minegusta.hat")) {
-				ItemStack i = p.getInventory().getItemInMainHand().clone();
-				ItemStack head = p.getInventory().getHelmet().clone();
+				ItemStack i = p.getInventory().getItemInMainHand() != null ? p.getInventory().getItemInMainHand().clone() : new ItemStack(Material.AIR);
+				ItemStack head = p.getInventory().getHelmet() != null ? p.getInventory().getHelmet().clone() : new ItemStack(Material.AIR);
 
 				p.getInventory().setItemInMainHand(head);
 				p.getInventory().setHelmet(i);
